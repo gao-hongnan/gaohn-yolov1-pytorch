5f1b1059b2b08a5f3599a416d9c6c024a82ccf45
Gao Hongnan <reighns.sjr.sjh.gr.twt.hrj@gmail.com>
2022-10-10 17:33:00 +0800

    fix/build: fix open without read "r" argument, update README and dev_requirements

--- README.md
+++ README.md
@@ -6,4 +6,6 @@ by Hongnan Gao
 
 <h4 align="center">
   <a href="https://gao-hongnan.github.io/gaohn-mlops-docs/mlops_docs/developing/general_workflow/#packaging-and-setup">Packaging and Setup</a>
+  •
+  <a href="https://madewithml.com/courses/mlops/packaging/">Made With ML's Packaging and Setup</a>
 </h4>
--- setup.py
+++ setup.py
@@ -7,10 +7,10 @@ from setuptools import setup
 BASE_DIR = Path(__file__).parent
 
 # Load packages from requirements.txt
-with open(Path(BASE_DIR, "requirements.txt")) as file:
+with open(Path(BASE_DIR, "requirements.txt"), "r") as file:
     required_packages = [ln.strip() for ln in file.readlines()]
 
-with open(Path(BASE_DIR, "dev_requirements.txt")) as file:
+with open(Path(BASE_DIR, "dev_requirements.txt"), "r") as file:
     dev_packages = [ln.strip() for ln in file.readlines()]
 
 setup(
