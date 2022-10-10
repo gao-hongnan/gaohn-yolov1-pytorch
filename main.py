68a5bcc34e7185619a9c86e1b82b930b87c59b62
Gao Hongnan <reighns.sjr.sjh.gr.twt.hrj@gmail.com>
2022-10-10 18:52:38 +0800

    build: add pre-commit config file and update package for pre-commit in dev_requirements

--- main.py
+++ main.py
@@ -1,19 +1,5 @@
 def shhq(shhq_member: str = "hn"):
-    if shhq_member in [
-        "hn",
-        "cw",
-        "jun",
-        "lh",
-        "lz",
-        "mj",
-        "sz",
-        "wj",
-        "yj",
-        "zj",
-        "sa",
-        "bc",
-        "de",
-    ]:
+    if shhq_member in ["hn", "cw", "jun", "lh", "lz", "mj", "sz", "wj", "yj", "zj"]:
         return True
     else:
         return False
